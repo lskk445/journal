c355d1e8493d02f9e665416db87e8a307437432d
siva lokam <sivalokam@sivas-MacBook-Air.local>
2024-09-08 20:41:40 +0530

    db.php

--- includes/db.php
+++ includes/db.php
@@ -4,13 +4,7 @@ $servername = 'localhost';
 $username='root';
 $password = 'Abcd@1234';
 $dbname = 'journal_db';
-/*
-$servername = 'localhost';
-$username='u592999880_todo';
-$password = 'Password@123';
-$dbname = 'u592999880_todo';
 
-*/
 
 $con = new mysqli($servername,$username,$password,$dbname);
 
